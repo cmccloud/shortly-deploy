5bed5b30dd347f17685ab6464122891b5d787657
Hack Reactor Students <accounts@hackreactor.com>
2014-08-22 16:39:07 -0700

    Add concat, production.js test builds successfully

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      dist: {
+        src: [
+          './public/client/*.js',
+          './public/lib/*.js'
+        ],
+        dest: './public/dist/production.js'
+      }
     },
 
     mochaTest: {
